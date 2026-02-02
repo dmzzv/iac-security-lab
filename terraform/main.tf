991f257acd1e91a6e9406420ecc5ff96a8cbc5bb
dmzzv <srdjandomuz@gmail.com>
2026-02-02 13:25:43 +0000

    Fix: remove public SSH/HTTP ingress to satisfy policy gate

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,20 +6,6 @@ resource "aws_security_group" "insecure_sg" {
   name        = "insecure-sg"
   description = "Security group with bad practices"
 
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
   egress {
     from_port   = 0
     to_port     = 0
